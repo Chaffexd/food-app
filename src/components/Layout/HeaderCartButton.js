469749c9ef24315eee65ac403fc6bf281f77232a
chaffexd <shanechaffe@gmail.com>
2023-03-08 23:25:10 +0000

    added animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 import classes from '../Layout/HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
 
-  const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+  const { items } = cartContext;
+
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const buttonClass = `${classes.button} ${buttonIsHighlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if(items.length === 0) {
+      return;
+    }
+    setButtonIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+
+    // This is to cleanup the timer in the effect as we should clean up
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClass} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
